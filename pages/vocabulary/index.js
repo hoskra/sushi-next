21c448c32739fcf2abbbc2e5bf20f654b9e58ab8
hoskra <hoskra@gmail.com>
2021-12-03 20:14:24 +0100

    faked vocabulary search

--- pages/vocabulary/index.js
+++ pages/vocabulary/index.js
@@ -13,6 +13,12 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import { changeName } from "../../redux/pageNameSlice";
 
+function DDDdisplay({name}) {
+    console.log(name)
+  return (
+    <span>{name}</span>
+  )
+}
 
 export default function Vocabulary() {
   const dispatch = useDispatch();
@@ -24,6 +30,32 @@ export default function Vocabulary() {
   const [letter, setLetter] = useState('A');
   let txs = useSelector((state) => state.textbook.value)
   txs = Object.entries(txs).filter(textbook=>textbook[1].userId==0);
+
+  const [selected, setSelected] = useState("all");
+
+  let terms = []
+
+  txs.filter(textbook => {
+    let x = {
+      id: textbook[1].id,
+      pages: []
+    }
+
+    textbook[1].pages.forEach(page => {
+      x.pages.push(page.selected)
+    })
+
+    terms.push(x)
+  });
+
+  let ter = {
+    0: terms.filter(te => te.id == 0)[0]?.pages,
+    3: terms.filter(te => te.id == 3)[0]?.pages,
+    4: terms.filter(te => te.id == 4)[0]?.pages,
+    5: terms.filter(te => te.id == 5)[0]?.pages,
+    6: terms.filter(te => te.id == 6)[0]?.pages,
+    7: terms.filter(te => te.id == 7)[0]?.pages,
+  }
 
   let createArrayAtoZ = _ => {
     return Array
@@ -52,6 +84,16 @@ export default function Vocabulary() {
     })
   }
 
+  let options = [{value: 'all', label: '-- All --'}];
+  let optionsTxs = txs.map(it => ({
+    value: it[1].id,
+    label: it[1].title
+  }))
+
+  optionsTxs.forEach(element => {
+      options.push(element)
+  });
+
   return (
     <>
       <HeadComponent />
@@ -62,15 +104,12 @@ export default function Vocabulary() {
             <FontAwesomeIcon icon={faSearch} />
             <input className="sushi-input" placeholder="search" />
           </div>
-          <div className={styles.group}>
+          {/* <div className={styles.group}>
             <span>Only added by me</span>
             <Toggle />
-          </div>
-          <Select height='80px' styles={customStyles} placeholder="Pick a textbook" className={styles.pageMenu} 
-            options={txs.map(it => ({
-              value: it[1].id,
-              label: it[1].title
-            }))}
+          </div> */}
+          <Select height='80px' styles={customStyles} placeholder="Pick a textbook" className={styles.pageMenu}
+            options={options} onChange={(e) => setSelected(e.value)}
             />
           <div>
             <button className="sushi-button" onClick={() => router.back()}>Back</button>
@@ -85,9 +124,16 @@ export default function Vocabulary() {
               })
             }
           </nav>
-          {[...Array(20)].map((x, i) => {
+          { selected == 'all' || ![0, 3, 4, 5, 6, 7].includes(selected) ?
+          [...Array(5)].map((x, i) => {
             return <p key={`term_${i}`}>{`${letter}_term${i}`}</p>
-          })}
+          }) :
+
+            ter[selected].map(te => {
+              return <p key={`term_${te}`}>{te}</p>
+            })
+
+        }
         </div>
       </div>
     </>
